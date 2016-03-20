b6fb20c2aa99e370782ae9e3b45e83aa7b84f2a4
Igor <rogi54321@yandex.ru>
2016-03-20 20:22:22 +0300

    1.8 Web-сервера

---
+++ init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mv /etc/nginx/sites-available/default /etc/nginx/sites-available/default1
+
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
